78f74273e6a15bbda71d257dd7963a6596fe1c99
wmsoftinformatica <welton.wmsoft@gmail.com>
2021-05-04 22:24:06 -0300

    inclusao do flyway e SQL criar e registrar categoria

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,10 @@
+CREATE TABLE categoria (
+	codigo BIGINT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmacia');
+INSERT INTO categoria (nome) values ('Outros');
