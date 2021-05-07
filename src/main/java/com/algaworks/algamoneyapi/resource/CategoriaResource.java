fe2552ea448c9136ecd47415c93c6d235a840f07
wmsoftinformatica <welton.wmsoft@gmail.com>
2021-05-06 23:30:15 -0300

    CADASTRAR NOVA CATEGORIA(COM ERROS)

--- src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
@@ -1,14 +1,25 @@
 package com.algaworks.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoneyapi.model.Categoria;
 import com.algaworks.algamoneyapi.repository.CategoriaRepository;
+//import com.algaworks.algamoneyapi.repository.CategoriaRepository;
 
 
 
@@ -23,7 +34,26 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
-		
+				
 	}
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		 Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				 .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		 response.setHeader("Location", uri.toASCIIString());
+		 
+		 return  ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+//	@GetMapping("/{codigo}")
+//	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+//		return categoriaRepository.findOne(codigo);
+//	}
+//	
+	
+	
 
 }
