502aade4160b6c768c2c7f4cf01270cac6250adc
welton maximiano <welton.wmsoft@gmail.com>
2021-05-24 11:14:13 -0300

    POST pessoa incompleto

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,12 +1,19 @@
 
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
@@ -22,6 +29,19 @@ public class PessoaResource {
 	private List<Pessoa> listar() {
 
 		return pessoaRepository.findAll();
+	}
+
+	@PostMapping
+
+	public ResponseEntity<Pessoa> criar(@RequestBody Pessoa pessoa, HttpServletResponse response) {
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+				.path("/{codigo}")
+				.buildAndExpand(pessoaSalva.getCodigo())
+				.toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		return ResponseEntity.created(uri).body(pessoaSalva);
+
 	}
 
 }
