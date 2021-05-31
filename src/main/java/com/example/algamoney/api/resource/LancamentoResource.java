b32967dc39a3f7eb85e6a8b520a678a65ed621b5
welton maximiano <welton.wmsoft@gmail.com>
2021-05-31 18:00:57 -0300

    video aula 5.9 02:23 incompleta

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -49,7 +50,7 @@ public class LancamentoResource {
 	private MessageSource messageSource;
 
 	@GetMapping
-	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
+	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		return lancamentoRepository.filtrar(lancamentoFilter); // listar todos os dados//
 	}
 
