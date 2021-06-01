5c32b459e638306856736a73adfb288f6eb250bb
welton maximiano <welton.wmsoft@gmail.com>
2021-06-01 17:42:39 -0300

    video aula 6.3 com erros

--- src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -1,31 +1,29 @@
 
 package com.example.algamoney.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerConfig extends OAuth2ResourceServerAutoConfiguration {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ROLE");
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/categorias")
 				.permitAll()
 				.anyRequest()
 				.authenticated()
-				.and()
-				.httpBasic()
 				.and()
 				.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -33,5 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.csrf()
 				.disable();
 	}
+
+	configure
+
 
 }
